32601090e894e97d965e984a8aaadf5e591c3f4c
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2023-04-03 22:33:19 -0700

    Build all versions of a given code version

--- docker/build-ci.sh
+++ docker/build-ci.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
-VERSION="$1"
+VERSION="1.0"
 
-if [ -z "$VERSION" ]; then
-    echo "Must specify a version to build"
-    exit 1;
-fi
+CI_BASE="ghcr.io/alignmentresearch/polygames:${VERSION}-ci-base"
+CI_SANITIZE="ghcr.io/alignmentresearch/polygames:${VERSION}-ci-sanitize"
+CI_RELWITHDEBINFO="ghcr.io/alignmentresearch/polygames:${VERSION}-ci-relwithdebinfo"
+RUNNER="ghcr.io/alignmentresearch/polygames:${VERSION}-runner"
 
-echo "Building version $VERSION"
-docker build -t "ghcr.io/alignmentresearch/polygames:${VERSION}-ci-base" \
-    -f docker/ci-base/Dockerfile .
-docker build --build-arg "POLYGAMES_VERSION=${VERSION}" \
-    -t "ghcr.io/alignmentresearch/polygames:${VERSION}-ci-sanitize" \
-    -f docker/ci-sanitize/Dockerfile .
-docker build --build-arg "POLYGAMES_VERSION=${VERSION}" \
-    -t "ghcr.io/alignmentresearch/polygames:${VERSION}-ci-relwithdebinfo" \
-    -f docker/ci-relwithdebinfo/Dockerfile .
+echo "Building or pulling version $VERSION for CI"
+docker pull "$CI_BASE" \
+    || { docker build -t "$CI_BASE" -f docker/ci-base/Dockerfile . \
+    && docker push "$CI_BASE"; }
 
+docker pull "$CI_SANITIZE" \
+    || { docker build --build-arg "POLYGAMES_VERSION=${VERSION}" \
+    -t "$CI_SANITIZE" -f docker/ci-sanitize/Dockerfile . \
+    && docker push "$CI_SANITIZE"; }
 
-docker push "ghcr.io/alignmentresearch/polygames:${VERSION}-ci-base"
-docker push "ghcr.io/alignmentresearch/polygames:${VERSION}-ci-sanitize"
-docker push "ghcr.io/alignmentresearch/polygames:${VERSION}-ci-relwithdebinfo"
+docker pull "$CI_RELWITHDEBINFO" \
+    || { docker build --build-arg "POLYGAMES_VERSION=${VERSION}" \
+    -t "$CI_RELWITHDEBINFO" -f docker/ci-relwithdebinfo/Dockerfile . \
+    && docker push "$CI_RELWITHDEBINFO"; }
+
+echo "Building or pulling version $VERSION for the experiment runner"
+docker pull "$RUNNER" \
+    || { docker build --build-arg "POLYGAMES_VERSION=${VERSION}" \
+    -t "$RUNNER" -f docker/runner/Dockerfile . \
+    && docker push "$RUNNER"; }
+
+# Get the SSH agent's current keys
+SSH_KEY="$(ssh-add -L)"
+echo "Building the devbox for version $VERSION and SSH key $SSH_KEY"
+docker build --build-arg "SSH_KEY=${SSH_KEY}" \
+    --build-arg "POLYGAMES_VERSION=${VERSION}" \
+    -t "$DEVBOX" -f docker/devbox/Dockerfile .
