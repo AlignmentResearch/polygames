b933c7e913ffd7208329ad343ad7b76a4a3db72d
Niki Howe <nikihowe@gmail.com>
2023-04-04 02:28:25 +0000

    Remove human_mode flag, which appears to do nothing

--- pypolygames/human.py
+++ pypolygames/human.py
@@ -69,7 +69,6 @@ def create_human_environment(
         pure_mcts=pure_mcts,
         actor_channel=actor_channel,
         model_manager=None,
-        human_mode=True,
         total_time=total_time,
         time_ratio=time_ratio,
         sample_before_step_idx=80,
@@ -128,7 +127,6 @@ def create_tp_environment(
         pure_mcts=pure_mcts,
         actor_channel=actor_channel,
         model_manager=None,
-        human_mode=True,
         total_time=total_time,
         time_ratio=time_ratio,
     )
