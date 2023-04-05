11c690d5711da163258022ecd1e3bc25ba94d21b
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2023-04-04 17:31:56 -0700

    Right order for module definitions

--- src/core/pybind.cc
+++ src/core/pybind.cc
@@ -23,12 +23,10 @@ using namespace core;
 PYBIND11_MODULE(polygames, m) {
   m.def("init_threads", &threads::Threads::init);
 
-  auto m_mcts = m.def_submodule("mcts", "the MCTS algorithm");
-  mcts::pybind_submodule(m_mcts);
-
   auto m_tube = m.def_submodule("tube", "Store replay buffers and networking");
   tube::pybind_submodule(m_tube);
 
+  // This references tube::EnvThread so tube module must be defined before
   py::class_<Game, tube::EnvThread, std::shared_ptr<Game>>(m, "Game")
       .def(py::init<std::string, std::vector<std::string>, int, int, bool, bool, bool, bool, bool, int,
                     int, bool, int, int, bool, int>())
@@ -95,4 +93,10 @@ PYBIND11_MODULE(polygames, m) {
       .def("set_find_batch_size_max_bs", &ModelManager::setFindBatchSizeMaxBs);
 
   py::class_<SampleResult>(m, "SampleResult").def("get", &SampleResult::get);
+
+
+  // References core::Actor so must go after this file's main defs
+  auto m_mcts = m.def_submodule("mcts", "the MCTS algorithm");
+  mcts::pybind_submodule(m_mcts);
+
 }
