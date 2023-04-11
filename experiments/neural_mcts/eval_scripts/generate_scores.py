677232aa92d31be902058dd24cec007178d81ff4
Niki Howe <nikihowe@gmail.com>
2023-04-10 20:07:50 -0700

    Automate 0-to-2048 rollout mcts opponents

--- experiments/neural_mcts/eval_scripts/generate_scores.py
+++ experiments/neural_mcts/eval_scripts/generate_scores.py
@@ -4,19 +4,24 @@ import pathlib
 import sys
 
 
-def make_command(model_path, opponent_path, save_dir):
+mcts_rollouts = [0, 32, 64, 128, 256, 512, 1024, 2048]
+
+
+def run_against_several_MCTS_opponentsn(model_path, save_dir):
+    for num_pure_mcts_opponent_rollouts in mcts_rollouts:
+        command = make_command(model_path, num_pure_mcts_opponent_rollouts, save_dir)
+        print(f"Running {command}")
+        os.system(command)
+
+
+def make_command(model_path, num_pure_mcts_opponent_rollouts, save_dir):
     command_list = []
     command_list.append(f"python -m pypolygames eval")
     command_list.append(f"--checkpoint {model_path}")
     command_list.append(f"--num_game_eval 300")
     command_list.append(f"--num_rollouts_eval 1")
     command_list.append(f"--sample_before_step_idx 0")
-    if mcts_opponent:
-        num_rollouts = opponent_path.split('=')[1]
-        command_list.append(f"--num_rollouts_opponent {num_rollouts}")
-    if not mcts_opponent:
-        command_list.append(f"--checkpoint_opponent {opponent_path}")
-        command_list.append(f"--num_rollouts_opponent 1")
+    command_list.append(f"--num_rollouts_opponent {num_pure_mcts_opponent_rollouts}")
     # Save the output to out.txt and the error to err.txt
     command_list.append(f"2>> {save_dir}/err.txt")
     command_list.append(f">> {save_dir}/out.txt")
@@ -26,23 +31,17 @@ def make_command(model_path, opponent_path, save_dir):
 
 def generate_scores(the_args):
 
-    if not len(sys.argv) == 4:
-        print("Usage: python generate_scores.py checkpoint_path opponent_path save_dir")
+    if not len(the_args) == 3:
+        print("Usage: python generate_scores.py checkpoint_path save_dir")
         sys.exit()
-        
-    _, checkpoint_dir, opponent_path, save_dir = sys.argv
+
+    _, checkpoint_dir, opponent_path, save_dir = the_args
 
     if checkpoint_dir[-1] == "/":
         checkpoint_dir = checkpoint_dir[:-1]
 
     if save_dir[-1] == "/":
         save_dir = save_dir[:-1]
-
-    if "MCTS" in opponent_path or "mcts" in opponent_path:
-        assert("=" in opponent_path)
-        mcts_opponent = True
-    else:
-        mcts_opponent = False
 
     models = os.listdir(checkpoint_dir)
     models = filter(lambda x: ".pt" in x, models)
@@ -68,4 +67,4 @@ def generate_scores(the_args):
 
 
 if __name__ == "__main__":
-    generate_scores(sys.argv)
+    _, model_path, save_dir = sys.argv
