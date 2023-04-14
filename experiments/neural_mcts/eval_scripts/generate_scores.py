8fcfea721abd4e19e05ebe0da8263f4c0652bf05
Niki Howe <nikihowe@gmail.com>
2023-04-13 19:21:27 -0700

    Temporarily switch to new branch

--- experiments/neural_mcts/eval_scripts/generate_scores.py
+++ experiments/neural_mcts/eval_scripts/generate_scores.py
@@ -7,6 +7,12 @@ from utils import make_command
 
 
 def generate_scores(model_dir, num_pure_mcts_opponent_rollouts, save_dir):
+
+    # Get us to the correct git repo
+    subprocess.run("cd /polygames", shell=True)
+    subprocess.run("git checkout run_pure_mcts_experiments", shell=True)
+    subprocess.run("git pull", shell=True)
+
     if model_dir[-1] == "/":
         model_dir = model_dir[:-1]
 
