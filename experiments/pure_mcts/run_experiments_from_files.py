85f46b032130fc3cf3f3952a4198d71105ee927f
Niki Howe <nikihowe@gmail.com>
2023-04-17 07:16:45 -0700

    Attempt to improve directory scanning

--- experiments/pure_mcts/run_experiments_from_files.py
+++ experiments/pure_mcts/run_experiments_from_files.py
@@ -9,16 +9,22 @@ def get_docker_commands_from_directories(root_directory):
     # off of the root directory. For each of those files, run "run_from_command_line_inputs" on the
     # command in the file.
 
-    for directory in sorted(os.listdir(root_directory)):
-        directory_path = os.path.join(root_directory, directory)
-        docker_command_file = os.path.join(directory_path, "docker_command.txt")
-        if os.path.isfile(docker_command_file):
-            with open(docker_command_file, "r") as f:
+    for item in sorted(os.scandir(root_directory), key=lambda x: x.name.lower()):
+        if item.is_dir():
+            directory_path = item.path
+            docker_command_file = os.path.join(directory_path, "docker_command.txt")
+            if os.path.isfile(docker_command_file):
+                with open(docker_command_file, "r") as f:
+                    docker_command = f.readlines()[0].strip()
+                print(f"Running {docker_command}...")
+                subprocess.run(shlex.split(docker_command))
+        elif item.is_file() and item.name == "docker_command.txt":
+            with open(item.path, "r") as f:
                 docker_command = f.readlines()[0].strip()
             print(f"Running {docker_command}...")
             subprocess.run(shlex.split(docker_command))
         else:
-            print(f"Skipping {directory_path} because it doesn't have a docker_command.txt file")
+            print(f"Skipping {item} because it doesn't have a docker_command.txt file")
 
 
 if __name__ == "__main__":
