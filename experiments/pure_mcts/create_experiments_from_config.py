f8c0d0a0eededb4da76dc6181789b0fd14b51ac3
Niki Howe <nikihowe@gmail.com>
2023-04-28 22:18:55 -0400

    Try to fix memory logging

--- experiments/pure_mcts/create_experiments_from_config.py
+++ experiments/pure_mcts/create_experiments_from_config.py
@@ -65,7 +65,7 @@ def setup_game_and_save_command(
     commands.append(f"git checkout {CURRENT_BRANCH}")
     commands.append(f"git branch --set-upstream-to=origin/{CURRENT_BRANCH} {CURRENT_BRANCH}")
     commands.append("git pull")
-    commands.append(f'/bin/bash /polygames/experiments/memory_tracking.sh {directory_path}/memory.log &')
+    commands.append(f'/bin/bash /polygames/experiments/memory_tracking.sh {directory_path} &')
     commands.append(f"(time {single_command}) > {directory_path}/time.log")
 
     with open(f"{directory_path}/run.sh", "w") as f:
